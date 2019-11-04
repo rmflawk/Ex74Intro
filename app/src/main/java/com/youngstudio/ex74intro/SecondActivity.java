667a0fd4a7f8597770d46e399b98aadea70ba644
rmflawk <ant921111@naver.com>
2019-11-04 17:45:07 +0900

    첫번째 커밋

---
+++ app/src/main/java/com/youngstudio/ex74intro/SecondActivity.java
@@ -0,0 +1,19 @@
+package com.youngstudio.ex74intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+
+
+    }
+
+}
